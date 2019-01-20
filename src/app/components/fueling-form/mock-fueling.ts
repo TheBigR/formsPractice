f63c907c4f4d2f5aec475b9712de3d973ec84199
Roy <roy@webiks.com>
2019-01-20 14:37:02 +0200

    added mock file

---
+++ src/app/components/fueling-form/mock-fueling.ts
@@ -0,0 +1,26 @@
+import {Fueling} from './fueling.model';
+
+export const FUELINGS: Fueling[] = [
+  {
+    id: 1,
+    name: 'pre jeepolog fueling',
+    description: 'The fueling before the jeepolog anual meeting. ',
+    location: {zone: 12, location_x: 555, location_y: 4576457},
+    ellipse: {width: 453, length: 33, azimuth: 43},
+    type: 'Diesel',
+    classification: 'Dor-Alon',
+    activity: 'Full',
+    certaintylevel: 'High'
+  },
+  {
+    id: 2,
+    name: 'weekly fueling',
+    description: 'just a weekly fueling. ',
+    location: {zone: 12, location_x: 555, location_y: 4576457},
+    ellipse: {width: 453, length: 33, azimuth: 43},
+    type: 'Diesel',
+    classification: 'Ten',
+    activity: 'Full',
+    certaintylevel: 'High'
+  }
+];
