7d952847e8840bb47eef21dd5f4cf82dde451c8d
TheBigR <nevoroy@gmail.com>
2019-06-27 18:02:00 +0300

    vaction functions on ts created

--- src/app/formsPracticeAppModule/modules/vehicle-action/components/v-action/v-action.component.ts
+++ src/app/formsPracticeAppModule/modules/vehicle-action/components/v-action/v-action.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { editVactionSelector } from '../../store/vAction.selector';
+import { distinctUntilChanged, tap } from 'rxjs/operators';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { SubmitForm, UpdateEditVaction } from '../../store/vAction.actions';
 
 @Component({
   selector: 'app-v-action',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VActionComponent implements OnInit {
 
-  constructor() { }
+  vActionForm;
+  subscriptions: Array<Subscription> = [];
+  isEditMode = true;
+
+  editVaction$ = this.store.pipe(
+    select(editVactionSelector),
+    tap(() => this.vActionForm ? null : this.setVactionForm()),
+    tap((vaction) => Object.keys(vaction)
+      .filter(key => Boolean(this.vActionForm.controls[key]))
+      .forEach(key => this.vActionForm.controls[key].setValue(vaction[key])))
+  );
+
+  constructor(private store: Store<any>) { }
+
+  setVactionForm() {
+    this.vActionForm =  new FormGroup({
+      name: new FormControl(null, Validators.required),
+      type: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+      date: new FormControl(null, Validators.required),
+      vendor: new FormControl(null, Validators.required),
+    });
+    this.subscriptions.push(
+      ...Object.keys(this.vActionForm.controls).map( key =>
+        this.vActionForm.controls[key].valueChanges.pipe(
+          distinctUntilChanged(),
+          tap(value => this.store.dispatch(new UpdateEditVaction({vAction: <any>{[key]: value } })))
+        ).subscribe()
+      )
+    );
+  }
+
+  toggleEditMode() {
+    this.isEditMode = !this.isEditMode;
+  }
+
+  onSubmitForm() {
+    this.store.dispatch(new SubmitForm());
+  }
+
+  resetForm() {
+    this.vActionForm.reset();
+  }
 
   ngOnInit() {
+    this.subscriptions.push(
+      this.editVaction$.subscribe()
+    );
   }
 
 }
